64fa6fa6688430aa44e9a2f859444b2db3f1ff37
Aryan Rawat <rawataryan4338@gmail.com>
2025-08-03 07:09:02 +0000

    changes done

--- half_pyramid.cpp
+++ half_pyramid.cpp
@@ -29,6 +29,7 @@ int main(){
         cout<<endl;
 
     }
+    cout<<endl;
 
     for(int i=n-1;i>=0;i--){
         for(int j=0;j<=i;j++){
--- pattern_5.cpp
+++ pattern_5.cpp
@@ -9,3 +9,34 @@
 // ***
 // **
 // *
+
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    
+    int n;
+    cout<<"size";
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        // print i+1 stars
+        for(int j =0 ;j<=i ;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+
+
+}
