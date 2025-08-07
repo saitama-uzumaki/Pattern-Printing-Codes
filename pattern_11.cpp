fbc4450729df42fbba2ebb9837e786c49f2aecd0
Aryan Rawat <rawataryan4338@gmail.com>
2025-08-07 15:42:18 +0000

    added new pattern

---
+++ pattern_11.cpp
@@ -0,0 +1,23 @@
+#include<iostream> 
+using namespace std;
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E
+
+int main(){
+    int n;
+    cout<<"enter the size";
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        char ch = 'A'+i;
+        for(int j=0;j<=i;j++){
+            cout<<ch<<" ";       
+        }
+       
+        cout<<endl;
+    }
+
+}
